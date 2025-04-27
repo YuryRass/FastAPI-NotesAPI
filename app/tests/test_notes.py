9e8b05d3f4c9550dc85f6655cd3aaf13e64f9489
Yurii <yurchik1997yurchik@gmail.com>
2025-04-27 23:12:58 +0300

    used mocked method

--- app/tests/test_notes.py
+++ app/tests/test_notes.py
@@ -5,11 +5,12 @@ from app.utils.url import reverse
 
 
 async def test_success_add_and_get_note(
-    authenticated_ac: AsyncClient, true_content: str
+    authenticated_ac: AsyncClient,
+    mock_true_content: str,
 ) -> None:
     response: Response = await authenticated_ac.post(
         url=reverse(add_note_for_user),
-        json={"content": true_content},
+        json={"content": "true content"},
     )
 
     assert response.status_code == 200
@@ -17,15 +18,16 @@ async def test_success_add_and_get_note(
     response: Response = await authenticated_ac.get(url=reverse(get_notes))
 
     assert response.status_code == 200
-    assert response.json()[-1]["content"] == true_content
+    assert response.json()[-1]["content"] == "true content"
 
 
 async def test_unsuccess_add_note(
-    authenticated_ac: AsyncClient, invalid_content: str
+    authenticated_ac: AsyncClient,
+    mock_invalid_content: str,
 ) -> None:
     response: Response = await authenticated_ac.post(
         url=reverse(add_note_for_user),
-        json={"content": invalid_content},
+        json={"content": "any content"},
     )
 
     assert response.status_code == 422
